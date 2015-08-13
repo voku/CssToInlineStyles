046834fbd7135823e1fbe29f398f1e18f61982d7
Lars Moelleken <lmoelleken@menadwork.com>
2015-08-13 13:58:40 +0200

    [-]: clean-up "bootstrap.php" for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,3 @@
 <?php
-if (is_file(dirname(__DIR__) . '/src/voku/CssToInlineStyles.php')) {
-  require_once dirname(__DIR__) . '/src/voku/CssToInlineStyles.php';
-  require_once dirname(__DIR__) . '/src/voku/Exception.php';
-  require_once dirname(__DIR__) . '/src/voku/Specificity.php';
-} else {
-  require_once dirname(__DIR__) . '/vendor/autoload.php';
-}
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
